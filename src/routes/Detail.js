7569161dba2ede499744f63d1e83bbaf8f90ff92
Kangmin <mygangmini@naver.com>
2021-03-24 00:33:21 +0900

    [ADD] Movie component

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,29 @@
-export default () => "Dtail";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!) {
+        movie(id: $id) {
+            id
+            title
+            medium_cover_image
+            description_intro
+        }
+    }
+`;
+
+export default () => {
+    const { id } = useParams();
+    const { loading, data } = useQuery(GET_MOVIE, {
+        // variables: { id: +id } # str을 양의 int로 바꿔주는 법
+        variables: { id: Number(id) }
+    });
+    if (loading) {
+        return "loading";
+    }
+    if (data && data.movie) {
+        return data.movie.title;
+    }
+};
